51076a8d3a69ab4fd360f4656dbdc7207121b62e
ajlopez <webmaster@ajlopez.com>
2012-01-08 10:01:05 -0300

    Context with first test

---
+++ Src/AjRools.Expert/AjRools.Expert.Tests/ContextTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AjRools.Expert.Tests
+{
+    [TestClass]
+    public class ContextTests
+    {
+        private Context context;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            this.context = new Context();
+        }
+
+        [TestMethod]
+        public void GetUndefinedValueAsNull()
+        {
+            Assert.IsNull(this.context.GetValue("Foo"));
+        }
+    }
+}
---
+++ Src/AjRools.Expert/AjRools.Expert/Context.cs
@@ -0,0 +1,20 @@
+﻿namespace AjRools.Expert
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Context
+    {
+        public void SetValue(string name, object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object GetValue(string name)
+        {
+            return null;
+        }
+    }
+}
