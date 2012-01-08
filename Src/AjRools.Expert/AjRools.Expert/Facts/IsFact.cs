c8ac45337eeda9bcc3e401789ffdb027a27b7927
ajlopez <webmaster@ajlopez.com>
2012-01-08 18:08:32 -0300

    Refactor: IsFact inherits from NameVerbValueFact

--- Src/AjRools.Expert/AjRools.Expert/Facts/IsFact.cs
+++ Src/AjRools.Expert/AjRools.Expert/Facts/IsFact.cs
@@ -5,49 +5,11 @@
     using System.Linq;
     using System.Text;
 
-    public class IsFact : Fact
+    public class IsFact : NameVerbValueFact
     {
-        private string name;
-        private object value;
-
         public IsFact(string name, object value)
+            : base(name, "is", value)
         {
-            this.name = name;
-            this.value = value;
-        }
-
-        public string Name { get { return this.name; } }
-
-        public object Value { get { return this.value; } }
-
-        public override bool Equals(object obj)
-        {
-            if (obj == null)
-                return false;
-
-            if (!(obj is IsFact))
-                return false;
-
-            IsFact fact = (IsFact)obj;
-
-            if (this.name != fact.name)
-                return false;
-
-            if (this.value == null)
-                return fact.value == null;
-
-            if (!this.value.Equals(fact.value))
-                return false;
-
-            return true;
-        }
-
-        public override int GetHashCode()
-        {
-            if (this.value == null)
-                return this.name.GetHashCode();
-
-            return this.name.GetHashCode() * 17 + this.value.GetHashCode();
         }
     }
 }
---
+++ Src/AjRools.Expert/AjRools.Expert/Facts/NameVerbValueFact.cs
@@ -0,0 +1,60 @@
+﻿namespace AjRools.Expert.Facts
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class NameVerbValueFact : Fact
+    {
+        private string name;
+        private string verb;
+        private object value;
+
+        public NameVerbValueFact(string name, string verb, object value)
+        {
+            this.name = name;
+            this.verb = verb;
+            this.value = value;
+        }
+
+        public string Name { get { return this.name; } }
+
+        public string Verb { get { return this.verb; } }
+
+        public object Value { get { return this.value; } }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+
+            if (!(obj is NameVerbValueFact))
+                return false;
+
+            NameVerbValueFact fact = (NameVerbValueFact)obj;
+
+            if (this.name != fact.name)
+                return false;
+
+            if (this.verb != fact.verb)
+                return false;
+
+            if (this.value == null)
+                return fact.value == null;
+
+            if (!this.value.Equals(fact.value))
+                return false;
+
+            return true;
+        }
+
+        public override int GetHashCode()
+        {
+            if (this.value == null)
+                return this.name.GetHashCode();
+
+            return (this.name.GetHashCode() * 17 + this.verb.GetHashCode()) *13 + this.value.GetHashCode();
+        }
+    }
+}
