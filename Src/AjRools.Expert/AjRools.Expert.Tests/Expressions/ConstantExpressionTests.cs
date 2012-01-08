7725f69f64fd71c64fef44f51bc949b638555204
ajlopez <webmaster@ajlopez.com>
2012-01-08 10:08:51 -0300

    ConstantExpression with first tests

---
+++ Src/AjRools.Expert/AjRools.Expert.Tests/Expressions/ConstantExpressionTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AjRools.Expert.Expressions;
+
+namespace AjRools.Expert.Tests.Expressions
+{
+    [TestClass]
+    public class ConstantExpressionTests
+    {
+        [TestMethod]
+        public void CreateIntegerConstantExpression()
+        {
+            ConstantExpression expr = new ConstantExpression(1);
+            Assert.AreEqual(1, expr.Value);
+        }
+
+        [TestMethod]
+        public void EvaluateStringConstantExpression()
+        {
+            ConstantExpression expr = new ConstantExpression("Adam");
+            Assert.AreEqual("Adam", expr.Evaluate(null));
+        }
+    }
+}
---
+++ Src/AjRools.Expert/AjRools.Expert/Expressions/ConstantExpression.cs
@@ -0,0 +1,24 @@
+﻿namespace AjRools.Expert.Expressions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class ConstantExpression
+    {
+        private object value;
+
+        public ConstantExpression(object value)
+        {
+            this.value = value;
+        }
+
+        public object Value { get { return this.value; } }
+
+        public object Evaluate(Context context)
+        {
+            return this.value;
+        }
+    }
+}
