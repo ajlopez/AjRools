f12c679ad26677f445be1d2c56f3527e4d32acce
ajlopez <webmaster@ajlopez.com>
2012-01-08 11:48:48 -0300

    Rule with first tests ready to fire

---
+++ Src/AjRools.Expert/AjRools.Expert.Tests/Rules/RuleTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AjRools.Expert.Rules;
+using AjRools.Expert.Facts;
+
+namespace AjRools.Expert.Tests.Rules
+{
+    [TestClass]
+    public class RuleTests
+    {
+        [TestMethod]
+        public void RuleIsNotReadyInEmptyWorld()
+        {
+            Rule rule = new Rule(new Fact[] {
+                new IsFact("Temperature", 40),
+                new IsFact("Age", 50)
+            }, null);
+
+            World world = new World();
+
+            Assert.IsFalse(rule.IsReadyToFire(world));
+        }
+
+        [TestMethod]
+        public void RuleIsReadyToFire()
+        {
+            Rule rule = new Rule(new Fact[] {
+                new IsFact("Temperature", 40),
+                new IsFact("Age", 50)
+            }, null);
+
+            World world = new World();
+            world.AssertFact(new IsFact("Age", 50));
+            world.AssertFact(new IsFact("Temperature", 40));
+
+            Assert.IsTrue(rule.IsReadyToFire(world));
+        }
+    }
+}
---
+++ Src/AjRools.Expert/AjRools.Expert/Rules/Rule.cs
@@ -0,0 +1,29 @@
+﻿namespace AjRools.Expert.Rules
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using AjRools.Expert.Facts;
+
+    public class Rule
+    {
+        private IList<Fact> conditions;
+        private IList<Fact> assertions;
+
+        public Rule(IList<Fact> conditions, IList<Fact> assertions)
+        {
+            this.conditions = conditions;
+            this.assertions = assertions;
+        }
+
+        public bool IsReadyToFire(World world)
+        {
+            foreach (var condition in this.conditions)
+                if (!world.IsAFact(condition))
+                    return false;
+
+            return true;
+        }
+    }
+}
