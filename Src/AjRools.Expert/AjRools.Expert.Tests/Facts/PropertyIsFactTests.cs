64feed190c00f85889fd90ba8cc1ea111be9f935
ajlopez <webmaster@ajlopez.com>
2012-01-15 19:07:17 -0300

    PropertyIsFact with first tests

---
+++ Src/AjRools.Expert/AjRools.Expert.Tests/Facts/PropertyIsFactTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AjRools.Expert.Facts;
+
+namespace AjRools.Expert.Tests.Facts
+{
+    [TestClass]
+    public class PropertyIsFactTests
+    {
+        [TestMethod]
+        public void IsSatisfiedByStringLength()
+        {
+            PropertyIsFact fact = new PropertyIsFact("p", "Length", 3);
+            fact.IsSatisfiedByObject("123");
+        }
+
+        [TestMethod]
+        public void IsSatisfiedByStringInContext()
+        {
+            Context context = new Context();
+            context.SetValue("p", "123");
+            PropertyIsFact fact = new PropertyIsFact("p", "Length", 3);
+            fact.IsSatisfiedByContext(context);
+        }
+    }
+}
---
+++ Src/AjRools.Expert/AjRools.Expert/Facts/PropertyIsFact.cs
@@ -0,0 +1,34 @@
+﻿namespace AjRools.Expert.Facts
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Reflection;
+
+    public class PropertyIsFact
+    {
+        private string name;
+        private string property;
+        private object value;
+
+        public PropertyIsFact(string name, string property, object value)
+        {
+            this.name = name;
+            this.property = property;
+            this.value = value;
+        }
+
+        public bool IsSatisfiedByContext(Context context)
+        {
+            return this.IsSatisfiedByObject(context.GetValue(this.name));
+        }
+
+        public bool IsSatisfiedByObject(object @object)
+        {
+            Type type = @object.GetType();
+            PropertyInfo pinfo = type.GetProperty(this.property);
+            return pinfo.GetValue(@object, null).Equals(this.value);
+        }
+    }
+}
